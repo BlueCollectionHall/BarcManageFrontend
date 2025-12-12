a7c45a215497fc49b87af5bd53a672e1c1632603
Yasenbaka <1260352143@qq.com>
2025-12-12 14:01:17 +0800

    Update 评论、留言管理视图界面搭建、配置页面路由。

--- src/Routers/index.ts
+++ src/Routers/index.ts
@@ -4,8 +4,9 @@ import HomeRouter from "@/Routers/Router/HomeRouter.ts";
 import SignRouter from "@/Routers/Router/SignRouter.ts";
 import UserRouter from "@/Routers/Router/UserRouter.ts";
 import WorkRouter from "@/Routers/Router/WorkRouter.ts";
+import MessageRouter from "@/Routers/Router/MessageRouter.ts";
 
-const routes = [HomeRouter, SignRouter, UserRouter, WorkRouter];
+const routes = [HomeRouter, SignRouter, UserRouter, WorkRouter, MessageRouter];
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
